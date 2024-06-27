90946682b91be45b6e3334a33555e4634aaa2669
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-06-27 09:30:36 +0330

    implement customer services and rest apis

--- services/customer/src/main/java/com/micro/customer/service/CustomerService.java
+++ services/customer/src/main/java/com/micro/customer/service/CustomerService.java
@@ -1,17 +1,18 @@
 package com.micro.customer.service;
 
+import com.ctc.wstx.util.StringUtil;
 import com.micro.customer.exception.CustomerNotFoundException;
 import com.micro.customer.mapper.CustomerMapper;
 import com.micro.customer.model.ApiResponse;
+import com.micro.customer.model.Customer;
 import com.micro.customer.model.CustomerRequest;
 import com.micro.customer.model.CustomerResponse;
 import com.micro.customer.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 
 @Service
@@ -35,8 +36,23 @@ public class CustomerService {
 
     public ResponseEntity<ApiResponse<Long>> updateCustomer(CustomerRequest customerRequest) {
         var customer = customerRepository.findById(customerRequest.id()).orElseThrow(() -> new CustomerNotFoundException(
-                String.format("Cannot update customer :: no Customer found with this provided id :: %s" , customerRequest.id())
+                String.format("Cannot update customer :: no Customer found with this provided id :: %s", customerRequest.id())
         ));
+        mergeCustomer(customerRequest, customer);
         return null;
+    }
+
+    private void mergeCustomer(CustomerRequest customerRequest, Customer customer) {
+        if (StringUtils.isNotBlank(customerRequest.firstName()))
+            customer.setFirstName(customerRequest.firstName());
+
+        if (StringUtils.isNotBlank(customerRequest.lastName()))
+            customer.setLastName(customerRequest.lastName());
+
+        if (StringUtils.isNotBlank(customerRequest.email()))
+            customer.setEmail(customerRequest.email());
+
+        if (customerRequest.address() != null)
+            customer.setAddress(customerRequest.address());
     }
 }
