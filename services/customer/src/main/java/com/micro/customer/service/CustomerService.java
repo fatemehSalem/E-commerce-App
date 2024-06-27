86fc5444db980a586e3e336e14143f3ade3f991c
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-06-27 08:33:20 +0330

    add customer service

--- services/customer/src/main/java/com/micro/customer/service/CustomerService.java
+++ services/customer/src/main/java/com/micro/customer/service/CustomerService.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
