0329fd79e2d97a2fbd8d540b19fd9a46d059463f
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-07-08 12:56:31 +0330

    add Product services

--- services/product/src/main/java/com/micro/product/controller/ProductController.java
+++ services/product/src/main/java/com/micro/product/controller/ProductController.java
@@ -1,8 +1,6 @@
 package com.micro.product.controller;
 
-import com.micro.product.model.ApiResponse;
-import com.micro.product.model.ProductRequest;
-import com.micro.product.model.ProductResponse;
+import com.micro.product.model.*;
 import com.micro.product.service.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,8 +21,13 @@ public class ProductController {
 
     @PostMapping
     public ResponseEntity<ApiResponse<ProductResponse>> createProduct(@RequestBody @Valid ProductRequest productRequest){
-
         return productService.createProduct(productRequest);
+    }
 
+    @PostMapping("/purchase")
+    public ResponseEntity<ApiResponse<List<ProductPurchaseResponse>>> purchaseProducts(
+            @RequestBody  @Valid  List<ProductPurchaseRequest> request
+    ) {
+        return null;
     }
 }
