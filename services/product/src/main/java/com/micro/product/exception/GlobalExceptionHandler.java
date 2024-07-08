c0d9d0874eb45a3f7d768baa79c8053016046365
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-07-08 15:37:48 +0330

    implementation of purchase product

--- services/product/src/main/java/com/micro/product/exception/GlobalExceptionHandler.java
+++ services/product/src/main/java/com/micro/product/exception/GlobalExceptionHandler.java
@@ -1,9 +1,14 @@
 package com.micro.product.exception;
 
+import com.micro.product.model.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -12,5 +17,18 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handle(MethodArgumentNotValidException exception){
+        var errors = new HashMap<String , String>();
+        exception.getBindingResult().getAllErrors().forEach(objectError -> {
+            var fieldName = ((FieldError)objectError).getField();
+            var errorMessage = objectError.getDefaultMessage();
+            errors.put(fieldName , errorMessage);
+        });
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(errors));
     }
 }
