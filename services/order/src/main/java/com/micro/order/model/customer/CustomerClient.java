efa940206601bbd7195485e6dd6203c1dba6f7e7
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-08-12 12:06:43 +0330

    set up kafka

--- services/order/src/main/java/com/micro/order/model/customer/CustomerClient.java
+++ services/order/src/main/java/com/micro/order/model/customer/CustomerClient.java
@@ -1,8 +1,7 @@
 package com.micro.order.model.customer;
 
-import com.micro.order.model.ApiResponse;
+
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
