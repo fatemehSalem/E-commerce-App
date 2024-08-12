8c8e3e4d371681b44b6124bfae16310fd089adef
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-08-12 12:08:36 +0330

    set up kafka

--- services/order/src/main/java/com/micro/order/model/customer/CustomerClient.java
+++ services/order/src/main/java/com/micro/order/model/customer/CustomerClient.java
@@ -1,6 +1,7 @@
 package com.micro.order.model.customer;
 
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +12,12 @@ import java.util.Optional;
         name = "customer-service",
         url = "${application.config.customer-url}"
 )
+@CircuitBreaker(name = "myServiceCircuitBreaker", fallbackMethod = "fallbackMethod")
 public interface CustomerClient {
     @GetMapping("/findById/{customerId}")
     Optional<Long> findById(@PathVariable("customerId") Long customerId);
 
-
+    default String fallbackMethod(Long id, Throwable throwable) {
+        return "Fallback response test";
+    }
 }
