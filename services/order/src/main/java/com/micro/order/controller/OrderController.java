ec9f8716f00d5086f1e6f9e54a59c32d94826de3
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-10-19 09:51:06 +0330

    some changes in CustomerClient

--- services/order/src/main/java/com/micro/order/controller/OrderController.java
+++ services/order/src/main/java/com/micro/order/controller/OrderController.java
@@ -1,11 +1,9 @@
 package com.micro.order.controller;
 
-import com.micro.order.model.ApiResponse;
 import com.micro.order.model.OrderRequest;
 import com.micro.order.service.OrderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
